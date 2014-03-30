cdb8b0047b8b30d94046bb41aac3123961c3e2cd
nlantz <nicholaslantz@gmail.com>
2014-03-29 21:38:55 -0700

    Initial commit

---
+++ geoQuiz/src/main/java/com/nlantz/geoquiz/TrueFalse.java
@@ -0,0 +1,35 @@
+package com.nlantz.geoquiz;
+
+/**
+ * Created by nick on 3/29/14.
+ */
+public class TrueFalse {
+
+    private boolean mTrueQuestion;
+    private int mQuestion;
+
+
+    public TrueFalse(int question, boolean TrueQuestion){
+        mQuestion = question;
+        mTrueQuestion = TrueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+
+
+
+}
